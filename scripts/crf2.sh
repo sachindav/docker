526c13e8e5a88b059960912fbe9d89f755ee1de6
sachin.jha@accellier.com <sachin.jha@accellier.com>
2020-06-20 21:48:49 +0530

    d

--- scripts/crf2.sh
+++ scripts/crf2.sh
@@ -4,3 +4,6 @@
 sudo docker ps -aq
 sudo docker stop $(docker ps -aq)
 sudo docker rm $(docker ps -aq)
+
+cd /var/www/html/doc/node-bulletin-board/bulletin-board-app
+sudo docker build --tag bulletinboard:3.0 .
